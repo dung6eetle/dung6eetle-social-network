868a2b4ddbd17a7749ecbfd1992e59405f785357
dung6eetle <tem09970967@gmail.com>
2020-08-02 21:27:44 +0300

    refactoring Selectors

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -92,11 +92,11 @@ export const toggleFollowingProgress = (isFetching, userId) => ({
 
 // THUNK_CREATORs
 
-export const getUsersThunk = (currentPage, pageSize) => {
+export const getUsersThunk = (page, pageSize) => {
   return (dispatch) => {
     dispatch(toggleIsFetching(true));
-    dispatch(setCurrentPage(currentPage));
-    usersApi.getUsers(currentPage, pageSize).then((data) => {
+    dispatch(setCurrentPage(page));
+    usersApi.getUsers(page, pageSize).then((data) => {
       dispatch(toggleIsFetching(false));
       dispatch(setUsers(data.items));
       dispatch(setTotalUsersCount(data.totalCount));
