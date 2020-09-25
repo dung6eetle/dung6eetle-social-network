963547f3fbeb29dddb7aa48f9f25bf330102036f
dung6eetle <tem09970967@gmail.com>
2020-09-26 00:37:54 +0300

    refactoring

--- src/components/Profile/MyProfile/MyProfileData.js
+++ src/components/Profile/MyProfile/MyProfileData.js
@@ -1,17 +1,21 @@
 import React from "react";
-
+import classes from "./MyProfile.module.css";
 
 function MyProfileData({ profile, goToEditMode, isOwner }) {
   return (
     <>
-      {isOwner && <button onClick={goToEditMode}>edit</button>}
-      <div>
-        <div>Looking for a job: {profile.lookingForAJob ? "yes" : "no"}</div>
-        <div>Description:{profile.lookingForAJobDescription}</div>
-        <p>About me:{profile.aboutMe != null ? profile.aboutMe : <p>ABOUT ME..</p>}</p>
-      </div>
-      <div>
-        <div>
+      
+      <div className={classes.data}>
+        <div className={classes.data_top}>
+          <div>Description:{profile.lookingForAJobDescription}</div>
+          <div>Looking for a job: {profile.lookingForAJob ? "yes" : "no"}</div>
+        </div>
+        <div className={classes.data_center}>
+          <div>
+            About me:
+            {profile.aboutMe != null ? profile.aboutMe : <p>ABOUT ME..</p>}
+          </div>
+          <div>
           Contacts:
           {Object.keys(profile.contacts).map((key) => (
             <Contact
@@ -21,18 +25,27 @@ function MyProfileData({ profile, goToEditMode, isOwner }) {
             />
           ))}
         </div>
+        <div className={classes.data_bottom}>
+           <div className={classes.edit_bottom}>
+           {isOwner && <button onClick={goToEditMode}>edit</button>}
+           </div>
+        </div>
+        </div>
+      </div>
+      <div>
+        
       </div>
     </>
   );
 }
 export const Contact = ({ contactTitle, contactValue }) => {
-    return (
-      <>
-        <div>
-          {contactTitle} : {contactValue}
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div>
+        {contactTitle} : {contactValue}
+      </div>
+    </>
+  );
 };
 
 export default MyProfileData;
